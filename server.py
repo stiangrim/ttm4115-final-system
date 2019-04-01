761a2184ed1df22cc68875d243a06773f29fc99d
stiangrim <stiantg@stud.ntnu.no>
2019-04-01 13:37:20 +0200

    Added register function

--- server.py
+++ server.py
@@ -6,13 +6,6 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
-app.config['MQTT_BROKER_URL'] = '127.0.0.1'
-app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = 'user'
-app.config['MQTT_PASSWORD'] = 'secret'
-app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
-mqtt = Mqtt(app)
-
 # === MOCK DATABASE ====
 bathroom_queue = ["Anders", "Stian"]
 users = {
@@ -23,14 +16,17 @@ users = {
     "Aleksander": "pizza"
 }
 
+# === MQTT SETUP ===
+app.config['MQTT_BROKER_URL'] = '127.0.0.1'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = 'user'
+app.config['MQTT_PASSWORD'] = 'secret'
+app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+mqtt = Mqtt(app)
 
-# === REST API ===
-@app.route('/')
-def index():
-    return "<a href='/api/queue'>Check queue</a> "
 
-
-def get_queue():
+# === MQQT ===
+def publish_queue():
     mqtt.publish("client", json.dumps(bathroom_queue))
 
 
@@ -38,12 +34,20 @@ def login(username, password):
     if password == users[username]:
         mqtt.publish("client", json.dumps("Login successful!"))
     else:
-        mqtt.publish("client", json.dumps("wrong username or password"))
+        mqtt.publish("client", json.dumps("Wrong username or password"))
+
+
+def register_user(username, password):
+    if username in users:
+        mqtt.publish("client", json.dumps("User already exists. Try another username!"))
+    else:
+        users[username] = password
+        mqtt.publish("client", json.dumps("User: {} registered successfully!".format(username)))
 
 
 def add_user_to_queue(user):
     bathroom_queue.append(user)
-    get_queue()
+    publish_queue()
 
 
 def remove_user_from_queue(user):
@@ -65,12 +69,15 @@ def handle_mqtt_message(client, userdata, message):
     topic = message.topic
     payload = message.payload.decode()
     if topic == "queue":
-        get_queue()
+        publish_queue()
     elif topic == "queue/add":
         add_user_to_queue(payload)
     elif topic == "login":
         credentials = payload.split(":")
         login(credentials[0], credentials[1])
+    elif topic == "register":
+        credentials = payload.split(":")
+        register_user(credentials[0], credentials[1])
 
 
 if __name__ == '__main__':
