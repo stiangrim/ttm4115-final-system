5cfbce31882ef2f5a106f1cbad045a53d9d7005e
AleksanderSkarnes <aleksams@stud.ntnu.no>
2019-05-01 11:03:34 +0200

    Hardware interface is good

--- hardware.py
+++ hardware.py
@@ -1,20 +1,25 @@
 import RPi.GPIO as GPIO
+import pyttsx3
 
 BUTTON_PIN = 7
-BUTTON_PRESSED = false
+LED_PIN = 4
+DOOR_LOCKED = False
 
 
-def GPIO_setup():
+def GPIO_setup(method_to_run):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=my_callback, bouncetime=300)
-
-
-def my_callback(channel):
-    print("This is an edge event callback function")
-    BUTTON_PRESSED = not BUTTON_PRESSED
+    GPIO.setup(LED_PIN, GPIO.OUT)
+    GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=lambda x: my_callback(method_to_run), bouncetime=300)
 
 
 def GPIO_cleanup():
     GPIO.cleanup()
 
+
+def my_callback(method_to_run):
+    print("Button pressed!")
+    global DOOR_LOCKED
+    DOOR_LOCKED = not DOOR_LOCKED
+    method_to_run()
+    GPIO.output(LED_PIN, DOOR_LOCKED)
